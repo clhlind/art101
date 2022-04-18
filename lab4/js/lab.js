b1a11cc5da0fbd34053a647ef4ae1c4e2f72f143
clhlind <clairafreeze@icloud.com>
2022-04-18 13:26:21 -0700

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,19 @@
+/**
+ * Summary. (For this lab I learned how to use javascript and how to pseudocode. My partner and I worked together to complete this lab.)
+ *
+ * Description. (Our lab pseudocodes the game rock, paper, scissors.)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author ClairaLind.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
+//load hand model
+//Accept user input
+//Vaildate user's move
+//Evaluate possible moves
+//Choose computer moves
+//Show moves
+//Determine winner
