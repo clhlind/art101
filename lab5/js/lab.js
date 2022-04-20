c4e4fe74dba4499f86e273ce700a2ce315677098
clhlind <clairafreeze@icloud.com>
2022-04-20 13:12:34 -0700

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Name
+ * Created:   11.05.2009
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+ //Define Varibles
+ make = "mazda";
+ model = "cx5";
+ color = "navy";
+ year = 2015;
+
+ // calculate
+ age = 2022 - 2015
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "<br>");
+ 
